35b52f37105a534b15c00f9158fd87bed0b4422f
Spencer Dangel <spencerdangel@sevenhillstechnology.com>
2019-05-15 18:38:44 -0400

    Initial Commit

---
+++ swift-snapkit-demo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  swift-snapkit-demo
+//
+//  Created by Spencer Dangel on 5/15/19.
+//  Copyright © 2019 spencerdangel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ swift-snapkit-demoTests/swift_snapkit_demoTests.swift
@@ -0,0 +1,34 @@
+//
+//  swift_snapkit_demoTests.swift
+//  swift-snapkit-demoTests
+//
+//  Created by Spencer Dangel on 5/15/19.
+//  Copyright © 2019 spencerdangel. All rights reserved.
+//
+
+import XCTest
+@testable import swift_snapkit_demo
+
+class swift_snapkit_demoTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
